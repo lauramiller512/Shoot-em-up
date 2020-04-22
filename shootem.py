ccfaae281747409c0f4baad81cd625f6ca7ce1b5
lauramiller512 <lauramiller.spence@gmail.com>
2020-04-22 12:25:11 +0100

    Fixed health formula so health bar now displays correct amount of player health remaining

--- shootem.py
+++ shootem.py
@@ -45,7 +45,7 @@ def draw_shield_bar(surf, x, y, pct):
         pct = 0
     BAR_LENGTH = 100
     BAR_HEIGHT = 10
-    fill = (pct / 100) * BAR_LENGTH
+    fill = (player.shield * BAR_LENGTH) / 100
     outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
     fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
     pygame.draw.rect(surf, GREEN, fill_rect)
